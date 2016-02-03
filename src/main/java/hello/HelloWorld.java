3c8e4934202e26698e70a4c18c2e75eb28df078a
abenabderrahmane <ahmed.benabderrahmane@amadeus.com>
2016-02-03 17:22:43 +0100

    few lines commencted for the tuto

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -2,9 +2,14 @@ package hello;
 
 public class HelloWorld {
 	
+	
+     //private static final Logger                             LOG               = LoggerFactory.getLogger(HelloWorld.class);
+
+	// Public class
 	public static void main(String[] args) {
 
 		Greeter greeter = new Greeter();
 		System.out.println(greeter.sayHello());
+		//LOG.info("Message" , greeter.sayHello());
 	}
 }
